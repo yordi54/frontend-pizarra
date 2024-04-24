04bd58f6061c336d7ca4f3d13425dbcd639e78a1
yordi54 <yordice75@gmail.com>
2024-04-24 10:00:43 -0400

    actualizacion

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  url: 'postgres://software1:Aspirine1234@software1-sistema.postgres.database.azure.com:5432/sistema-software1',
+  url: 'postgres://yordice77:fLG7tnh0Ofcq5hmK5CzdZXFKYfFISgtK@dpg-cokgr9f79t8c73cad7pg-a.oregon-postgres.render.com/diagramador_app',
   synchronize: true,
   entities: ['dist/**/*.entity.{ts,js}'],
   autoLoadEntities: true,
